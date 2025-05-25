42a50a3eca348969e4b43b88dc3b84fba5cefdbf
lihaji <835510106@qq.com>
2025-05-25 14:11:13 +0800

    update

---
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -0,0 +1,83 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tv_result;
+
+    // 第一个操作数
+    private String firstNum = "";
+    // 运算符
+    private String operator = "";
+    // 第二个操作数
+    private String secondNum = "";
+    // 当前的计算结果
+    private String result = "";
+    // 显示的文本内容
+    private String showText = "";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calculator);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tv_result = findViewById(R.id.tv_result);
+
+        // 给每一个按钮注册点击监听器
+        findViewById(R.id.btn_cancel).setOnClickListener(this);
+        findViewById(R.id.btn_divide).setOnClickListener(this);
+        findViewById(R.id.btn_times).setOnClickListener(this);
+        findViewById(R.id.btn_clean).setOnClickListener(this);
+        findViewById(R.id.btn_seven).setOnClickListener(this);
+        findViewById(R.id.btn_eight).setOnClickListener(this);
+        findViewById(R.id.btn_nine).setOnClickListener(this);
+        findViewById(R.id.btn_four).setOnClickListener(this);
+        findViewById(R.id.btn_five).setOnClickListener(this);
+        findViewById(R.id.btn_six).setOnClickListener(this);
+        findViewById(R.id.btn_one).setOnClickListener(this);
+        findViewById(R.id.btn_two).setOnClickListener(this);
+        findViewById(R.id.btn_three).setOnClickListener(this);
+        findViewById(R.id.btn_x).setOnClickListener(this);
+        findViewById(R.id.btn_zero).setOnClickListener(this);
+        findViewById(R.id.btn_dot).setOnClickListener(this);
+        findViewById(R.id.btn_plus).setOnClickListener(this);
+        findViewById(R.id.btn_sub).setOnClickListener(this);
+        findViewById(R.id.btn_sqrt).setOnClickListener(this);
+        findViewById(R.id.btn_equals).setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String inputText;
+        
+
+        switch (v.getId()){
+            // 清除按钮
+            case R.id.btn_cancel:
+                break;
+
+        }
+
+    }
+}
