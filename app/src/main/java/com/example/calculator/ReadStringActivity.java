aa9195f9a47f4b3ccbb1cde4640dca79a57af406
lihaji <835510106@qq.com>
2025-05-28 02:03:52 +0800

    update

---
+++ app/src/main/java/com/example/calculator/ReadStringActivity.java
@@ -0,0 +1,31 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ReadStringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_read_string);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView tv_resource = findViewById(R.id.tv_resource);
+        // 从string.xml 中获取名叫 weather_str 的字符串
+        String value = getString(R.string.weather_str);
+        tv_resource.setText(value);
+    }
+}
